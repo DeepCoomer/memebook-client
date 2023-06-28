b769c7db26f994701de1cd9a3c96d94580f2dc90
DeepCoomer <deepcoomer198@gmail.com>
2023-06-28 22:26:40 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
-import Home from './Pages/home/Home';
-import Profile from './Pages/profile/Profile';
-import Login from './Pages/accounts/login/Login'
-import Register from './Pages/accounts/register/Register'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/home/Home";
+import Profile from "./Pages/profile/Profile";
+import Login from "./Pages/accounts/login/Login";
+import Register from "./Pages/accounts/register/Register";
+import { useViewportSize } from "@mantine/hooks";
 // import { getAllPosts } from './Services/api';
 
-
 function App() {
-
-  const [user, setuser] = useState({})
-  const [toggle, settoggle] = useState(false)
+  const [user, setuser] = useState({});
+  const [toggle, settoggle] = useState(false);
+  const { width } = useViewportSize();
 
   const getUser = async () => {
-    let response = await fetch("https://memebook-server.onrender.com/api/auth/getuser", {
-      method: 'POST',
-      headers: {
-        "auth-token": localStorage.getItem('token')
+    let response = await fetch(
+      "https://memebook-server.onrender.com/api/auth/getuser",
+      {
+        method: "POST",
+        headers: {
+          "auth-token": localStorage.getItem("token"),
+        },
       }
-    })
+    );
 
     let data = await response.json();
     setuser(data);
     // console.log(user);
-    settoggle(!toggle)
-  }
+    settoggle(!toggle);
+  };
 
   useEffect(() => {
+    if (width< 700) {
+      alert("Please open in desktop mode")
+    }
     getUser();
     // console.log(user.username)
-    // eslint-disable-next-line 
-  }, [])
+    // eslint-disable-next-line
+  }, []);
 
   useEffect(() => {
     // getUser();
     // console.log(user.username)
-    // eslint-disable-next-line 
-  }, [toggle])
+    // eslint-disable-next-line
+  }, [toggle]);
 
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home user={user} getUser={getUser} toggle={toggle} />} />
-          <Route exact path="/profile/:username" element={<Profile username={user.username} currentuser={user} />} />
+          <Route
+            exact
+            path="/"
+            element={<Home user={user} getUser={getUser} toggle={toggle} />}
+          />
+          <Route
+            exact
+            path="/profile/:username"
+            element={<Profile username={user.username} currentuser={user} />}
+          />
           <Route exact path="/accounts/login" element={<Login />} />
           <Route exact path="/accounts/register" element={<Register />} />
         </Routes>
